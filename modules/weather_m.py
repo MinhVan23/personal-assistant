0b4d56360e35c0b823263d421373e99151f9c713
MinhVan23 <thantrongminhvan@gmail.com>
2024-12-05 21:12:19 +0700

    added command weather, implemented weather module

--- modules/weather_m.py
+++ modules/weather_m.py
@@ -1,15 +1,25 @@
 import sys
 
-import requests
-from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 def weather_scrape():
     url='https://e.vnexpress.net/weather/tp-hcm'
-    web_data=requests.get(url)
-    html=web_data.text
-    soup=BeautifulSoup(html, 'html.parser')
-    print(soup.find('div', class_="name"))
-    sys.exit(0)
+    service=Service(ChromeDriverManager().install())
+    options=Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-dev-shm-usage')
+    driver=webdriver.Chrome(service=service, options=options)
+    driver.get(url)
+    element=WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@class="name"]')))
+    weather_status=element.text.lower()
+    driver.quit()
+    return weather_status
 
 def fetch_weather():
     weather_status=weather_scrape()
